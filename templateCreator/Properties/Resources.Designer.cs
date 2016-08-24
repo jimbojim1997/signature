10a04a6a8df0efa16be542b234261b47920415f5
jimbojim1997 <jimbojim1997@gmail.com>
2016-08-24 22:08:38 +0100

    Created new WPF project

--- templateCreator/Properties/Resources.Designer.cs
+++ templateCreator/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Signature.Properties
+namespace templateCreator.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace Signature.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Signature.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("templateCreator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- templateCreator/Properties/Settings.Designer.cs
+++ templateCreator/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Signature.Properties
+namespace templateCreator.Properties
 {
 
 
